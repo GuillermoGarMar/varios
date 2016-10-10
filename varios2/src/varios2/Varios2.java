3e0fb3fb454cd7244b04ffe3ef96497e222640f0
csantamariacameselle <csantamariacameselle@atanasoff19.danielcastelao.org>
2016-10-10 09:44:18 +0200

    Cambio a G

--- varios2/src/varios2/Varios2.java
+++ varios2/src/varios2/Varios2.java
@@ -16,7 +16,7 @@ public class Varios2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("r");
+        System.out.println("G");
     }
     
 }
